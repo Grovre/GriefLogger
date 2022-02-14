7596655243c737c5e1b03402ea64435aff98ebf1
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-14 15:05:05 -0600

    Base64 UUID encoder and decoder class

---
+++ src/main/java/me/grovre/grieflogger/UuidCoder.java
@@ -0,0 +1,25 @@
+package me.grovre.grieflogger;
+
+import java.io.*;
+import java.util.Base64;
+import java.util.UUID;
+
+public class UuidCoder {
+
+    public static String encodeToBase64(UUID uuid) throws IOException {
+        ByteArrayOutputStream bytearroutstr = new ByteArrayOutputStream();
+        ObjectOutputStream objoutstr = new ObjectOutputStream(bytearroutstr);
+        objoutstr.writeObject(uuid);
+        byte[] ba = bytearroutstr.toByteArray();
+        String encoded = Base64.getEncoder().encodeToString(ba);
+        return encoded;
+    }
+
+    public static UUID decodeFromBase64(String encoded) throws IOException, ClassNotFoundException {
+        byte[] ba = Base64.getDecoder().decode(encoded);
+        ByteArrayInputStream bytearrinstr = new ByteArrayInputStream(ba);
+        ObjectInputStream objinstr = new ObjectInputStream(bytearrinstr);
+        UUID decoded = (UUID) objinstr.readObject();
+        return decoded;
+    }
+}
