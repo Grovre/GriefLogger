030d5746f242f5e3638678dd51721f348ce1bbc6
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-14 14:18:49 -0600

    config creation and static plugin

--- src/main/java/me/grovre/grieflogger/GriefLogger.java
+++ src/main/java/me/grovre/grieflogger/GriefLogger.java
@@ -4,9 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GriefLogger extends JavaPlugin {
 
+    public static GriefLogger plugin;
+
+    public static GriefLogger getPlugin() {
+        return plugin;
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        plugin = this;
 
     }
 
